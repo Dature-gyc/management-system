5f0aa6707cb0463f67d35bde8b3d1c1a149b1229
213ljp <2363595516@qq.com>
2020-07-06 18:19:33 +0800

    添加路由

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+// 引入路由
+import router from "./router";
 
 
-
-import ElementUi from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUi from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUi);
-
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
